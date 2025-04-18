a10740f990c074b5b269f61fbd639ab498941b7a
Spartacus <83741703+spartacusjv@users.noreply.github.com>
2025-04-18 10:37:38 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo timedatectl set-timezone Asia/Kolkata
+sudo apt-get update && apt-get upgrade && apt-get autoremove && sudo apt-get clean -y
 sudo mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
