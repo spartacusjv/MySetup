98f1f7512a17ced3495d6e7a68bba39a792a4ba6
Spartacus <83741703+spartacusjv@users.noreply.github.com>
2025-04-18 10:54:35 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 sudo timedatectl set-timezone Asia/Kolkata
-sudo apt-get update && apt-get upgrade && apt-get autoremove && sudo apt-get clean -y
 sudo mkdir -p /etc/apt/keyrings
-wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+if [ -d /etc/apt/keyrings/gierens.gpg ]; then
+  echo "........"
+ else
+   wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+fi
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoclean -y
 sudo apt-get install fish fastfetch eza -y
 sudo apt update -y
 if [ -d ~/.local/share/omf ]; then
